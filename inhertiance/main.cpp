f250b5c7d6045ace16937099a6e60a98063c5d80
Deeer <mac@219.local>
2015-04-14 12:01:55 +0800

    Initial Commit

---
+++ inhertiance/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  inhertiance
+//
+//  Created by METC on 15/4/14.
+//  Copyright (c) 2015年 METC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
